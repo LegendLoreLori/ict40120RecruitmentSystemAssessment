f054d1af72f179241396cb5f1277008a3d04778b
lorenzo <20101642@tafe.wa.edu.au>
2023-05-04 16:30:02 +0800

    made classes for contractor, job made static class recruitment system edited mainwindow xaml

---
+++ ict40120RecruitmentSystemAssessment/Contractor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ict40120RecruitmentSystemAssessment
+{
+    public class Contractor
+    {
+        public string Id { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        public bool IsAssigned { get; set; }
+        public int HourlyWage { get; private set; }
+
+        public Contractor(string id, string firstName, string lastName, int hourlyWage)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            IsAssigned = false;
+            HourlyWage = hourlyWage;
+        }
+    }
+}
---
+++ ict40120RecruitmentSystemAssessment/Job.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ict40120RecruitmentSystemAssessment
+{
+    public class Job
+    {
+        public string Id { get; private set; }
+        public string Name { get; private set; }
+        public DateOnly CompletionDate { get; private set; }
+        public bool IsAssigned { get; set; }
+        public bool IsComplete { get; set; }
+
+        public Job(string id, string name, DateOnly completionDate) 
+        {
+            Id = id;
+            Name = name;
+            CompletionDate = completionDate;
+            IsAssigned = false;
+            IsComplete = false;
+        }
+    }
+}
